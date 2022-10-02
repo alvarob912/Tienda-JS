68300d22d3db81eabbc8fc741a097c19ee57fa3a
Alvaro Bijarra <alvarobijarra14@gmail.com>
2022-10-02 14:30:38 -0300

    codigo limpio

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
-const stock = [
-    {
-        "id" : 1,
-        "nombre": "Buzo Nike SB",
-        "img": "./resource/buzonike.jpg",
-        "precio": 18500,
-        "cantidad": 1
-    },
-    {
-        "id" : 2,
-        "nombre": "Remera Nike blanca 002",
-        "img": "./resource/remerablanca.jpg",
-        "precio": 8500,
-        "cantidad": 1
-    },
-    {
-        "id" : 3,
-        "nombre": "Remera Nike SB3",
-        "img": "./resource/remerablancaroja.jpg",
-        "precio": 8500,
-        "cantidad": 1
-    },
-    {
-        "id" : 4,
-        "nombre": "Pantalon Nike FJ",
-        "img": "./resource/pantalon.jpg",
-        "precio": 12500,
-        "cantidad": 1
-    },
-    {
-        "id" : 5,
-        "nombre": "Campera Nike gris",
-        "img": "./resource/camperagris.jfif",
-        "precio": 14000,
-        "cantidad": 1
-    },
-    {
-        "id" : 6,
-        "nombre": "Zapatillas Nike air 97 gris",
-        "img": "./resource/zapatillasnike.webp",
-        "precio": 31500,
-        "cantidad": 1
-    }
-]
+import { stock } from "./stock.js";
 
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    function renderizarProductos (){
+    const renderizarProductos = () => {
         
         //capturo el div
         const tienda = document.getElementById('tienda');
@@ -92,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-function agregarAlCarrito (id){
+const agregarAlCarrito = (id) =>{
     
     let producto = stock.find(producto => producto.id === id);
 
@@ -114,7 +71,7 @@ function agregarAlCarrito (id){
 
 }
 
-function renderizarCarrito(){
+const renderizarCarrito = () => {
 
     let carritoHTML = document.querySelector('#carrito');
 
@@ -151,7 +108,7 @@ function renderizarCarrito(){
     })
 }
 
-function EliminarCarrito(indice){
+const EliminarCarrito = (indice) =>{
 
     carrito[indice].cantidad--;
 
@@ -166,7 +123,7 @@ function EliminarCarrito(indice){
 }
 
 
-function calcularTotal(){
+const calcularTotal = () =>{
 
     let total = 0;
 
---
+++ stock.js
@@ -0,0 +1,46 @@
+const stock = [
+    {
+        "id" : 1,
+        "nombre": "Buzo Nike SB",
+        "img": "./resource/buzonike.jpg",
+        "precio": 18500,
+        "cantidad": 1
+    },
+    {
+        "id" : 2,
+        "nombre": "Remera Nike blanca 002",
+        "img": "./resource/remerablanca.jpg",
+        "precio": 8500,
+        "cantidad": 1
+    },
+    {
+        "id" : 3,
+        "nombre": "Remera Nike SB3",
+        "img": "./resource/remerablancaroja.jpg",
+        "precio": 8500,
+        "cantidad": 1
+    },
+    {
+        "id" : 4,
+        "nombre": "Pantalon Nike FJ",
+        "img": "./resource/pantalon.jpg",
+        "precio": 12500,
+        "cantidad": 1
+    },
+    {
+        "id" : 5,
+        "nombre": "Campera Nike gris",
+        "img": "./resource/camperagris.jfif",
+        "precio": 14000,
+        "cantidad": 1
+    },
+    {
+        "id" : 6,
+        "nombre": "Zapatillas Nike air 97 gris",
+        "img": "./resource/zapatillasnike.webp",
+        "precio": 31500,
+        "cantidad": 1
+    }
+]
+
+export { stock };
